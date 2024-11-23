af1d69a794583d34d1083f2d517386c1dfb30aaf
katharinazwick <katharinazwick.71@gmail.com>
2024-11-23 23:02:47 +0100

    fix kathis bug

--- src/modules/pointsForGame.js
+++ src/modules/pointsForGame.js
@@ -3,14 +3,17 @@ import {setForecast, setReached} from "./sharedState.js";
 export function pointsForGame(event) {
     const input = event.target;
     const formPointsForGame = new FormData(input.form);
+    console.log(formPointsForGame);
     const fc = []
     const rd = []
     for (let p = 0; p < 4; p++) {
         fc[p] = [];
         rd[p] = [];
         for (let i =0; i<3;i++){
-            fc[p][i] = formPointsForGame.get(`forecast${p}${i}`);
-            rd[p][i] = formPointsForGame.get(`reached${p}${i}`);
+            if (formPointsForGame.get(`forecast${p}${i}`) !== '') {
+                fc[p][i] = formPointsForGame.get(`forecast${p}${i}`);
+                rd[p][i] = formPointsForGame.get(`reached${p}${i}`);
+            }
         }
     }
     //console.log(fc);
